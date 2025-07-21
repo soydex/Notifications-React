4b0627dc3758dffd8fb54bb046750dded7f6f5c5
soydex <49359256+soydex@users.noreply.github.com>
2025-07-21 19:54:19 +0200

    chore: update package.json for deployment and add gh-pages dependency; configure Vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Notifications-React/",
   plugins: [react(), tailwindcss()],
   assetsInclude: ["**/*.md"],
 });
